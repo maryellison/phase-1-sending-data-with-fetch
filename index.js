a2396247c608977d0aca00bcfd409e7e262dcd70
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-13 14:57:37 -0500

    commit tree ef003b7c06552edea7f19b313d1aff0f25a537c2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+const body = document.getElementsByTagName("body"[0]);
+
+function submitData(firstName, email) {
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObject)
+    .then(resp => resp.json())
+    .then(body => addNewContact(body.id))
+    .catch(error => document.querySelector('body').innerHTML = error)
+}
+
+function addNewContact(newId) {
+    document.getElementsByTagName("body")[0].innerHTML = newId;
+    document.body.append('Unauthorized Access')
+}
+
+submitData("Name", "name@name.com");
